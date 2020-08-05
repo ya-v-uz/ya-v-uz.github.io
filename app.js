1cc4d394d13f35ba72694abcf2a2be1ffd016e25
yavuz <56271907+ya-v-uz@users.noreply.github.com>
2020-08-05 16:18:05 +0300

    website publishing
    testing mobile responsiveness of my unfinished website

---
+++ app.js
@@ -0,0 +1,68 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    let navbarOpen = false;
+    
+   
+     
+     
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+       
+        
+    
+    
+    });
+    
+   
+    
+ 
+ 
+ }
+ 
+
+    const socialSlide = () => {
+    let social = false; 
+    var skype = document.getElementById("skype")
+    var discord = document.getElementById("discord") 
+    var mail = document.getElementById("mail") 
+    var  telegram = document.getElementById("telegram") 
+    var spotify = document.getElementById("spotify") 
+   
+    const contactblock = document.querySelector(".contactmeblock");
+    const contactactive = document.querySelector(".contactmeblock-active");
+
+
+    skype.addEventListener("hover", () => {
+        contactactive.classList.toggle("contactmeblock-active");
+        social = true;
+    
+    
+    });
+
+
+ } 
+
+  
+
+  
+  
+
+     
+
+
+     
+     
+  
+  
+  
+ 
+ 
+ 
+
+ navSlide();
+ 
+ 
+
+
+ 
